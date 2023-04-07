c601d6d1a8cae68fe4ad226a1487cc15ec168b22
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-04-07 09:58:07 +0700

    Add project files.

---
+++ easd_1_129_D/easd_1_129_D.cpp
@@ -0,0 +1,11 @@
+// 1.sangat di butuhkan karna dari setiap masalah harus di analisa terlebih dahulu satu persatu supaya dapat menentukan penyelesaian yang sesuai dari masalah tersebut
+// 2.struktur data dibedakan menjadi dua yaitu 
+  //-statik (ukuran tetap,tidak bisa berubah) array digunakan saat struktur datanya sudah tertentu atau valid
+  //-dinamis (fleksibel bisa di tambah dan di kurang) belum di ketahui data nya
+// 3.faktor faktor yang mempengaruhi efisiensi waktu suatu program 
+  //-kecepatan processor
+  //-compiler yang bertugas sebagai menerjemahkan dari bahasa pemrograman menjadibahasa komputer
+  //-operatiing system
+  //-bahasa pemrograman
+  //-ukuran sesuai input
+//
